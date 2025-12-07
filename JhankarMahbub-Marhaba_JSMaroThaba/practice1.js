d32ca44e0659f9387416f9dac6fb15b0763bf8b8
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-12-07 00:59:19 -0800

    Jhankar Mahbub ar books ar practise 1,2

---
+++ JhankarMahbub-Marhaba_JSMaroThaba/practice1.js
@@ -0,0 +1,15 @@
+const year = 2003;
+const bnS = 0;
+const students = 72;
+const marks = 70;
+const rooms = 5;
+const hours = 5;
+const total = 0 + 1 + 6 + 4 + 2 + 3 + 4 + 7 + 6 + 3 + 8;
+
+console.log(year);
+console.log(bnS);
+console.log(students);
+console.log(marks);
+console.log(rooms);
+console.log(hours);
+console.log(total);
---
+++ JhankarMahbub-Marhaba_JSMaroThaba/practice2.js
@@ -0,0 +1,16 @@
+const age = 22;
+const booksToRead = 8;
+let wakeUpTime = 9;
+wakeUpTime = 7;
+const wastedTime = 0.5;
+const borrow = 5;
+const goals = 6;
+const pillow = 1;
+
+console.log(age);
+console.log(booksToRead);
+console.log(wakeUpTime);
+console.log(wastedTime);
+console.log(borrow);
+console.log(goals);
+console.log(pillow);
