51f91bd6a29780bac75d16b5d9e2e54ed9d24760
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-12-16 01:14:20 -0800

    Jhankar Bhai book Practice 3 done

---
+++ JhankarMahbub-Marhaba_JSMaroThaba/practice3.js
@@ -0,0 +1,15 @@
+const name = "Raihan Habib Akash";
+const address = "Mirpur-6, Dhaka-1216";
+const varcity = "Titumir";
+const fan = "Sddie Sank";
+const movieOfTheYear = "Stranger Things";
+const hobby = "Waching Movies";
+const colors = "Green";
+
+console.log(name);
+console.log(address);
+console.log(varcity);
+console.log(fan);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(colors);
