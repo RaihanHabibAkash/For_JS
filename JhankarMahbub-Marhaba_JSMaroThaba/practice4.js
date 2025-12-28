caf3e108fa8cdc40358ff66fa142011e4cfa4842
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-12-27 19:30:35 -0800

    practice 4 up of js ar maro thaba

---
+++ JhankarMahbub-Marhaba_JSMaroThaba/practice4.js
@@ -0,0 +1,23 @@
+const frndHappy = true;
+const crushMsg = false;
+const celebrating = true;
+const isStudyDone = false;
+const isPhoneCharged = true;
+const isDinnerDone = false; 
+function program1 () {
+    const isHappy = true;
+    const hasMoney = false;
+    const isHungry = true;
+    console.log(isHappy);
+    console.log(hasMoney);
+    console.log(isHungry);
+} 
+program1(); 
+
+console.log(frndHappy);
+console.log(crushMsg);
+console.log(celebrating);
+console.log(isStudyDone);
+console.log(isPhoneCharged);
+console.log(isDinnerDone);
+
